425a79141f949ae9349fbb48a1d93d2fa294e6b8
Sinan Bekar <sinanbekar.work@gmail.com>
2022-10-25 17:42:28 +0300

    dndkit hello world

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-import Tiptap from "../components/Tiptap";
+import React, { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { Draggable } from "../components/Draggable";
+import { Droppable } from "../components/Droppable";
 
 export default function Home() {
-  return <Tiptap />;
+  const [parent, setParent] = useState(null);
+  const draggable = <Draggable id="draggable">Go ahead, drag me.</Draggable>;
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!parent ? draggable : null}
+      <Droppable id="droppable">
+        {parent === "droppable" ? draggable : "Drop here"}
+      </Droppable>
+    </DndContext>
+  );
+
+  function handleDragEnd({ over }: any) {
+    setParent(over ? over.id : null);
+  }
 }
